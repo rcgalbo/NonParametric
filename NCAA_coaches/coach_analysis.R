b1ebee7b4fbdaa2159056783195315c879cdabdc
Rick Galbo <rcglb627@gmail.com>
2017-01-18 15:21:04 -0600

    Delete coach_analysis.R

--- NCAA_coaches/coach_analysis.R
+++
@@ -1,8 +0,0 @@
-#jim_winner?? we'll find out
-
-#Question: Is Jim Boeheim an overrated coach?
-
-dat = read.csv('jim_good/CollegeCoaches.csv')
-
-#do a ton of dplyr stuff becase this data set isnt great
-library(dplyr)
